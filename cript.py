17602a4eb93063573c5dfd0d60e92281b989c545
mandala21 <lucasresone@gmail.com>
2019-05-23 13:03:17 -0300

    first commit

---
+++ cript.py
@@ -0,0 +1,11 @@
+from flags import alphabet
+
+class CriptJulio(object):
+    def __init__(self,n):
+        """
+        Inicia a classe, informe o numero de casas
+        n: Int
+        """
+        self.n = n
+    
+    
